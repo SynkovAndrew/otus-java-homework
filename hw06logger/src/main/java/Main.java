c5827c928c234c2ffa7a4c8b5ce98e5f71b75b70
Andrey Synkov <shark734@yandex.ru>
2019-05-13 10:03:45 +0300

    map with reflection analysis results's been added

--- hw06logger/src/main/java/Main.java
+++ hw06logger/src/main/java/Main.java
@@ -1,20 +1,18 @@
-import common.ProxyUtils;
-
 public class Main {
     public static void main(String[] args) {
-        // jdk proxy
-        final var iCalculator = ProxyUtils.createProxyUsingJDK();
+        final var iCalculator = jdk_proxy_impl.CalculatorProxy.get();
 
         iCalculator.multiplyWithTwo(2);
         iCalculator.add(1, 3);
+        iCalculator.add(1, 4);
         iCalculator.multiplyWithThree(3);
         iCalculator.addThreeIntegers(4, 5, 7);
 
-        // cglib proxy
-        final var calculator = ProxyUtils.createProxyUsingCglib();
+        final var calculator = cglib_impl.CalculatorProxy.get();
 
         calculator.multiplyWithTwo(2);
         calculator.add(1, 3);
+        calculator.add(1, 4);
         calculator.multiplyWithThree(3);
         calculator.addThreeIntegers(4, 5, 7);
     }
